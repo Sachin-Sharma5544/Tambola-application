207c9271f4e9ea8ff9c9b24832c7daeff7c2027b
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2022-11-16 15:21:20 +0530

    complete number display section

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let h1 = document.querySelector("h1");
-console.log(h1);
+let numDisplay = document.querySelector(".my-current-num");
+
+let id = setInterval(
+    function () {
+        numDisplay.textContent = Math.round(Math.random() * 10);
+    },
+    1000,
+    setTimeout(function () {
+        clearInterval(id);
+    }, 20000)
+);
