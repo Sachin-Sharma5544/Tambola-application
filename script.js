f01c9767464c61384745a99a16fdda5577a8c596
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2022-11-16 18:30:09 +0530

    complete called num section styling

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let numDisplay = document.querySelector(".my-current-num");
 
 let id = setInterval(
     function () {
-        numDisplay.textContent = Math.round(Math.random() * 10);
+        numDisplay.textContent = Math.round(Math.random() * 100);
     },
     1000,
     setTimeout(function () {
+        numDisplay.textContent = "End";
         clearInterval(id);
     }, 20000)
 );
